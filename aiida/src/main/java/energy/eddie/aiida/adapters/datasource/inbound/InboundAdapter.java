51f649c613c2441200eb171fb92bc3aac73249ef
Tobias Fischer <tobias@tformatix.at>
2026-03-02 08:32:21 +0100

    GH-2105 Fix formatting in inbound adapter
    Co-authored-by: Florian Weingartshofer <florian.weingartshofer@tuta.io>

--- aiida/src/main/java/energy/eddie/aiida/adapters/datasource/inbound/InboundAdapter.java
+++ aiida/src/main/java/energy/eddie/aiida/adapters/datasource/inbound/InboundAdapter.java
@@ -39,7 +39,8 @@ public class InboundAdapter extends MqttDataSourceAdapter<InboundDataSource> {
         acknowledgementStreamer = new InboundAcknowledgementStreamer(
                 mapper,
                 dataSource.acknowledgementTopic(),
-                inboundRecordSink.asFlux());
+                inboundRecordSink.asFlux()
+        );
     }
 
     @Override
